c5b68c48fd3bd9fa6603045348299bfcfeb480b3
supershowwei <supershowwei@gmail.com>
2022-05-25 12:24:03 +0800

    first commit

---
+++ NetCoreCefSharpLab/NetCoreCefSharpLab/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using CefSharp;
+using CefSharp.SchemeHandler;
+using CefSharp.WinForms;
+
+namespace NetCoreCefSharpLab
+{
+    public partial class Form1 : Form
+    {
+        private readonly ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            var settings = new CefSettings();
+
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+
+            settings.RegisterScheme(
+                new CefCustomScheme
+                {
+                    SchemeName = "local",
+                    DomainName = "shiseido",
+                    SchemeHandlerFactory = new FolderSchemeHandlerFactory(@"views", defaultPage: "index.html")
+                });
+
+            Cef.Initialize(settings);
+
+            this.browser = new ChromiumWebBrowser("local://shiseido/") { Dock = DockStyle.Fill };
+
+            this.Controls.Add(this.browser);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            this.browser.Dispose();
+
+            Cef.Shutdown();
+
+            base.OnFormClosing(e);
+        }
+    }
+}

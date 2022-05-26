7d17576740f231d838e690f8300a52e829fedaf6
johnny <johnny@wantgoo.com>
2022-05-26 19:18:36 +0800

    refactoring

--- NetCoreCefSharpLab/NetCoreCefSharpLab/views/binding.js
+++ NetCoreCefSharpLab/NetCoreCefSharpLab/views/binding.js
@@ -2,6 +2,6 @@ window.objectBound = (async function (bindingName) {
 
     await CefSharp.BindObjectAsync(bindingName);
     
-    Object.defineProperty(window, 'viewBinding', { get: function () { return this[bindingName]; } });
+    Object.defineProperty(window, "viewBinding", { get: function () { return this[bindingName]; } });
     
 })(`${window.location.pathname.replace(/\.[^\/]+$/g, "")}#viewBinding`);

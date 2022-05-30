0f92faf8e0c4cfd925c4b488ff3b97ebed292b2e
supershowwei <supershowwei@gmail.com>
2022-05-31 07:45:02 +0800

    refactoring

--- NetCoreCefSharpLab/NetCoreCefSharpLab/Form1.cs
+++ NetCoreCefSharpLab/NetCoreCefSharpLab/Form1.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.JavascriptBinding;
-using CefSharp.SchemeHandler;
 using CefSharp.WinForms;
 using NetCoreCefSharpLab.ViewBindings;
 
@@ -11,37 +10,35 @@ namespace NetCoreCefSharpLab
 {
     public partial class Form1 : Form
     {
-        private readonly ChromiumWebBrowser browser;
-
         public Form1()
         {
             this.InitializeComponent();
 
-            this.browser = new ChromiumWebBrowser { Dock = DockStyle.Fill };
+            var browser = new ChromiumWebBrowser { Dock = DockStyle.Fill };
 
-            this.browser.JavascriptObjectRepository.NameConverter = new CamelCaseJavascriptNameConverter();
+            browser.JavascriptObjectRepository.NameConverter = new CamelCaseJavascriptNameConverter();
 
-            this.browser.JavascriptObjectRepository.ResolveObject += (sender, e) =>
+            browser.JavascriptObjectRepository.ResolveObject += (sender, e) =>
                 {
-                    e.ObjectRepository.Register(e.ObjectName, ViewBindingFactory.Instance.Create(e.ObjectName, this.browser));
+                    e.ObjectRepository.Register(e.ObjectName, ViewBindingFactory.Instance.Create(e.ObjectName, browser));
                 };
 
-            this.browser.ConsoleMessage += (sender, args) =>
+            browser.ConsoleMessage += (sender, args) =>
                 {
                     Debug.WriteLine(args.Message);
                 };
 
-            this.browser.LoadingStateChanged += async (sender, args) =>
+            browser.LoadingStateChanged += async (sender, args) =>
                 {
                     if (args.IsLoading == false)
                     {
-                        var res = await this.browser.EvaluateScriptAsync("getNestedObjectList();");
+                        var res = await browser.EvaluateScriptAsync("getNestedObjectList();");
                     }
                 };
 
-            this.browser.LoadUrl("local://shiseido/");
+            browser.LoadUrl("local://shiseido/");
 
-            this.Controls.Add(this.browser);
+            this.Controls.Add(browser);
         }
     }
 }
